56ab07040c87fb3d56dae2305b70331bb8332700
luis11181 <luis11181@hotmail.com>
2022-02-17 09:42:44 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React, { Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Layout from "./components/layout/Layout";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+import "./App.css";
+
+import { Counter } from "./pages/CounterPage";
+import AuthPage from "./pages/AuthPage";
+import NotFound from "./pages/NotFound";
+import { Btc } from "./pages/Btc";
+
+function App() {
+  return (
+    <Layout>
+      {
+        //* suspense with  React.lazy(() => import("./pages/NewQuote")) let us only load the component that is being deployed at first, so it will only load the initial page or the one we visit or want, and later the other ones
+      }
+      <Routes>
+        <Route path="/" element={<Btc />} />
+        <Route path="/signIn" element={<AuthPage />} />
+        <Route path="/crear-comprobante" element={<AuthPage />} />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Layout>
+  );
+}
+
+export default App;
---
+++ src/pages/Btc.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+import { useAppSelector, useAppDispatch } from "../app/hooks";
+
+export function Btc() {
+  const dispatch = useAppDispatch();
+
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
